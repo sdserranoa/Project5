88eef41c2da91a1cc2b3f2abfd818427b0ca6ef7
Andres Donoso <af.donoso@uniandes.edu.co>
2020-05-27 19:05:40 -0500

    Finished home

--- app-back/front/src/index.js
+++ app-back/front/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+//CSS
+import './css/index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+//Bootstrap
+import Container from 'react-bootstrap/Container'
+
+//Components
+import Home from './components/home/home'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Container fluid>
+      <Home />
+    </Container>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register()
